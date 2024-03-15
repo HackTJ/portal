a659918da8a5debba3eacdd93eeac781df7ab9a7
Krishnan Shankar <krishnans2006@gmail.com>
2024-03-15 18:12:47 +0000

    Remove cookie functionality in color scheme logic (color.js)

--- portal/static/js/color.js
+++ portal/static/js/color.js
@@ -1,12 +1,6 @@
 function setTheme() {
-    const cookieTheme = getCookie('theme');
-    if (cookieTheme) {
-        $('html').attr('data-bs-theme', cookieTheme);
-    } else {
-        const browserColor = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-        $('html').attr('data-bs-theme', browserColor);
-        setCookie('theme', browserColor);
-    }
+    const browserColor = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    $('html').attr('data-bs-theme', browserColor);
 }
 
 setTheme();
@@ -14,5 +8,4 @@ setTheme();
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
     const newColorScheme = e.matches ? 'dark' : 'light';
     $('html').attr('data-bs-theme', newColorScheme);
-    setCookie('theme', newColorScheme);
 });
